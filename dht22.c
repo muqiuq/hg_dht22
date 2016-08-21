6f3ad1576ab54909b0088c954014cfdd3110dcc5
muqiuq <philipp@uisa.ch>
2016-08-21 03:28:10 +0200

    Removed mergin conflicts.

--- dht22.c
+++ dht22.c
@@ -5,11 +5,7 @@
  *	Amended by technion@lolware.net
  */
 
-<<<<<<< HEAD
 #include <wiringX.h>
-=======
-#include <wiringPi.h>
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +16,6 @@
 #include "locking.h"
 
 #define MAXTIMINGS 85
-<<<<<<< HEAD
 static int DHTPIN = 1;
 static int dht22_dat[5] = {0,0,0,0,0};
 
@@ -28,11 +23,6 @@ static void nullprint(int prio, const char *format_str, ...) {
 
 }
 
-=======
-static int DHTPIN = 7;
-static int dht22_dat[5] = {0,0,0,0,0};
-
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
 static uint8_t sizecvt(const int read)
 {
   /* digitalRead() and friends from wiringpi are defined as returning a value
@@ -40,11 +30,7 @@ static uint8_t sizecvt(const int read)
 
   if (read > 255 || read < 0)
   {
-<<<<<<< HEAD
     //printf("Invalid data from wiringPi library\n");
-=======
-    printf("Invalid data from wiringPi library\n");
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
     exit(EXIT_FAILURE);
   }
   return (uint8_t)read;
@@ -61,15 +47,9 @@ static int read_dht22_dat()
   // pull pin down for 18 milliseconds
   pinMode(DHTPIN, OUTPUT);
   digitalWrite(DHTPIN, HIGH);
-<<<<<<< HEAD
   delayMicroseconds(10000);
   digitalWrite(DHTPIN, LOW);
   delayMicroseconds(18000);
-=======
-  delay(10);
-  digitalWrite(DHTPIN, LOW);
-  delay(18);
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
   // then pull it up for 40 microseconds
   digitalWrite(DHTPIN, HIGH);
   delayMicroseconds(40); 
@@ -112,28 +92,19 @@ static int read_dht22_dat()
         if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
 
 
-<<<<<<< HEAD
     //printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t );
     printf("{\"humidity\":\"%.2f\",\"temperature\":\"%.2f\"}\n", h, t );
-=======
-    printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t );
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
     return 1;
   }
   else
   {
-<<<<<<< HEAD
     //printf("Data not good, skip\n");
-=======
-    printf("Data not good, skip\n");
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
     return 0;
   }
 }
 
 int main (int argc, char *argv[])
 {
-<<<<<<< HEAD
   wiringXLog = nullprint;
   int lockfd;
   int tries = 30;
@@ -141,13 +112,6 @@ int main (int argc, char *argv[])
   if (argc < 2) {
     /*printf ("usage: %s <pin> (<tries>)\ndescription: pin is the wiringPi pin number\nusing 7 (GPIO 4)\nOptional: tries is the number of times to try to obtain a read (default 100)",argv[0]);*/
   }
-=======
-  int lockfd;
-  int tries = 100;
-
-  if (argc < 2)
-    printf ("usage: %s <pin> (<tries>)\ndescription: pin is the wiringPi pin number\nusing 7 (GPIO 4)\nOptional: tries is the number of times to try to obtain a read (default 100)",argv[0]);
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
   else
     DHTPIN = atoi(argv[1]);
    
@@ -156,7 +120,6 @@ int main (int argc, char *argv[])
     tries = atoi(argv[2]);
 
   if (tries < 1) {
-<<<<<<< HEAD
     exit(EXIT_FAILURE);
   }
 
@@ -165,17 +128,6 @@ int main (int argc, char *argv[])
   lockfd = open_lockfile(LOCKFILE);
 
   if (wiringXSetup () == -1)
-=======
-    printf("Invalid tries supplied\n");
-    exit(EXIT_FAILURE);
-  }
-
-  printf ("Raspberry Pi wiringPi DHT22 reader\nwww.lolware.net\n") ;
-
-  lockfd = open_lockfile(LOCKFILE);
-
-  if (wiringPiSetup () == -1)
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
     exit(EXIT_FAILURE) ;
 	
   if (setuid(getuid()) < 0)
@@ -186,17 +138,10 @@ int main (int argc, char *argv[])
 
   while (read_dht22_dat() == 0 && tries--) 
   {
-<<<<<<< HEAD
      delayMicroseconds(1000000); // wait 1sec to refresh
   }
 
   delayMicroseconds(1500000);
-=======
-     delay(1000); // wait 1sec to refresh
-  }
-
-  delay(1500);
->>>>>>> e3656bfd5c9676408f741b62e4bcb78bb4722a59
   close_lockfile(lockfd);
 
   return 0 ;
